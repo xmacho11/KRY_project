89d6b2f99a4d3b244978e1af6f5cda5a9c970841
Radim Macho <221666@vut.cz>
2025-03-04 14:05:27 +0100

    initial creation of main.py file for project

---
+++ main.py
@@ -0,0 +1,9 @@
+##############################################
+#                KRY_project				 #
+#											 #
+# authors: Radim Macho		ID: 221666		 #
+#		   Vilem Pechacek  	ID: 			 #
+##############################################
+
+if __name__ == "__main__":
+	print("Make out projet work")
