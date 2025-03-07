938986ff74dfd015e7b5b479beec6718f0310959
Vilém Pecháček <pechy.v@gmail.com>
2025-03-07 21:36:34 +0100

    Delete Program directory

--- Program/main.py
+++
@@ -1,9 +0,0 @@
-##############################################
-#                KRY_project				 #
-#											 #
-# authors: Radim Macho		ID: 221666		 #
-#		   Vilem Pechacek  	ID: 			 #
-##############################################
-
-if __name__ == "__main__":
-	print("Make out projet work")
